67c31a7ce718f3f8b2522bdf17736f4b1f89dd1d
Roland Tubongye W <roland.beni1@gmail.com>
2023-04-21 01:36:06 +0200

    [UPDATE] product complete routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ProductAddComponent } from './products/product-add.component';
+import { ProductSingleComponent } from './products/product-single.component';
 import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
   { path: '', component: AppComponent },
-  { path: 'products', component: ProductsComponent},
+  {
+    path: 'products',
+    children: [
+      {
+        path: '',
+        component: ProductsComponent
+      },
+      {
+        path: 'add',
+        component: ProductAddComponent
+      },
+      {
+        path: ':id',
+        component: ProductSingleComponent
+      }
+    ]
+  },
 ];
 
 @NgModule({
