0f9b63ec30cf0848037aafac51201a99cfdd2e83
Roland Tubongye W <roland.beni1@gmail.com>
2023-04-21 01:22:58 +0200

    [ADD] products' list route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'products', component: ProductsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
