c060831daaf6c1c0f5198c9596d3a761216fb884
Roland Beni Tubongye W <roland.beni1@gmail.com>
2023-04-22 01:04:18 +0200

    Front end : Product Add View (#1)
    * [UPDATE]
    # product add form(with form controls) and some action like submit
    # uid module
    
    * [UPDATE] import ReactiveFormModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProductComponent } from './products/product.component';
-import { ProductsComponent } from './products/products.component';
-import { ProductAddComponent } from './products/product-add.component';
-import { ProductSingleComponent } from './products/product-single.component';
-import { HeaderComponent } from './header/header.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ProductComponent} from './products/product.component';
+import {ProductsComponent} from './products/products.component';
+import {ProductAddComponent} from './products/product-add.component';
+import {ProductSingleComponent} from './products/product-single.component';
+import {HeaderComponent} from './header/header.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -20,9 +21,12 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
