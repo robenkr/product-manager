70bcac8cca8b22ca57c4b88609e1a4159e4df565
Roland Tubongye W <roland.beni1@gmail.com>
2023-04-24 17:27:40 +0200

    [UPDATE] connect to api and edit the product

--- src/app/products/product-edit.component.ts
+++ src/app/products/product-edit.component.ts
@@ -1,44 +1,51 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Product} from "../models/product.model";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Subscription} from "rxjs";
 import {ActivatedRoute} from "@angular/router";
+import {ProductsService} from "../services/products.service";
 
 @Component({
   selector: 'app-product-edit',
   templateUrl: './product-edit.component.html',
   styleUrls: ['./product-edit.component.css']
 })
-export class ProductEditComponent implements OnInit {
+export class ProductEditComponent implements OnInit, OnDestroy {
 
   productForm: FormGroup = new FormGroup([]);
-  productDraft: Product = {
-    id: '1',
-    name: 'Product 1',
-    description: ' Product 1 - description',
-    price: 10,
-    available: true,
-    imageUrl: 'https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-    quantity: 100,
-    storeName: 'Ampire Store',
-    createdAt: new Date(),
-    updatedAt: null
-  };
+  // @ts-ignore
+  product: Product;
   subscriptions: Subscription = new Subscription();
-  productId: string | null = '';
+  productId: string = '';
+  updated: boolean = false;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+              private productService: ProductsService) {
   }
 
   ngOnInit(): void {
     this.initForm();
     this.route.paramMap.subscribe(
       token => {
-        this.productId = token.get('id');
+        // @ts-ignore
+        this.productId = token.get('uid');
+        this.subscriptions.add(
+          this.productService.getProduct(this.productId).subscribe(
+            (product) => {
+              if (product !== null) {
+                this.product = product;
 
-        this.patchForm();
+                this.patchForm(product);
+              }
+            }
+          )
+        );
       }
     );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
   initForm(): void {
@@ -46,37 +53,44 @@ export class ProductEditComponent implements OnInit {
       name: new FormControl('', [Validators.required, Validators.minLength(3)]),
       description: new FormControl('', [Validators.minLength(10)]),
       price: new FormControl(0, [Validators.required, Validators.min(0)]),
-      imageUrl: new FormControl('', Validators.required),
+      image: new FormControl(''),
       quantity: new FormControl(0, [Validators.required, Validators.min(0)]),
       available: new FormControl(false, Validators.required),
-      storeName: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      store: new FormControl('', [Validators.required, Validators.minLength(5)]),
 
     });
   }
 
-  patchForm(): void {
+  patchForm(product: Product): void {
     this.productForm.patchValue({
-      name: this.productDraft.name,
-      description: this.productDraft.description,
-      price: this.productDraft.price,
-      imageUrl: this.productDraft.imageUrl,
-      quantity: this.productDraft.quantity,
-      available: this.productDraft.available,
-      storeName: this.productDraft.storeName
+      name: product.name,
+      description: product.description,
+      price: product.price,
+      quantity: product.quantity,
+      available: product.available,
+      store: product.store
     });
   }
 
   onSubmit(): void {
-    this.productDraft.name = (this.productForm.get('name')?.value).trim();
-    this.productDraft.description = (this.productForm.get('description')?.value).trim();
-    this.productDraft.price = this.productForm.get('price')?.value;
-    this.productDraft.quantity = this.productForm.get('quantity')?.value;
-    this.productDraft.available = this.productForm.get('available')?.value;
-    this.productDraft.storeName = this.productForm.get('storeName')?.value;
-    this.productDraft.imageUrl = this.productForm.get('imageUrl')?.value;
-    this.productDraft.updatedAt = new Date();
+    this.product.name = (this.productForm.get('name')?.value).trim();
+    this.product.description = (this.productForm.get('description')?.value).trim();
+    this.product.price = this.productForm.get('price')?.value;
+    this.product.quantity = this.productForm.get('quantity')?.value;
+    this.product.available = this.productForm.get('available')?.value;
+    this.product.store = this.productForm.get('store')?.value;
+    if (this.productForm.get('image')?.value !== '') {
+      this.product.image = this.productForm.get('image')?.value;
+    }
+    this.product.updated_at = new Date();
 
-    console.log(this.productDraft);
+    console.log(this.product);
+    this.subscriptions.add(
+      this.productService.updateProduct(this.product).subscribe(
+        (res) => {
+          console.log('#After Update> ', res);
+        })
+    );
   }
 
 }
