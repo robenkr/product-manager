dbff37d80b9d47a8ee540445d13543cfe3e5caf4
Roland Tubongye W <roland.beni1@gmail.com>
2023-04-24 17:29:45 +0200

    [UPDATE] change some attributs' name in model and update informations in views and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
         component: ProductAddComponent
       },
       {
-        path: ':id',
+        path: ':uid',
         children: [
           {
             path: '',
