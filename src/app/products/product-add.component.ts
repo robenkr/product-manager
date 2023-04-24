b420442df03498d52a52a86708d54f3670b60379
Roland Tubongye W <roland.beni1@gmail.com>
2023-04-24 17:28:20 +0200

    [UPDATE] connect to api and add the product in database

--- src/app/products/product-add.component.ts
+++ src/app/products/product-add.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Product} from "../models/product.model";
 import {uid} from "uid";
+import {ProductsService} from "../services/products.service";
 
 @Component({
   selector: 'app-product-add',
@@ -13,7 +14,7 @@ export class ProductAddComponent implements OnInit {
   productForm: FormGroup = new FormGroup([]);
   uid: string = '';
 
-  constructor() {
+  constructor(private productsService: ProductsService) {
   }
 
   ngOnInit(): void {
@@ -26,28 +27,33 @@ export class ProductAddComponent implements OnInit {
       name: new FormControl('', [Validators.required, Validators.minLength(3)]),
       description: new FormControl('', [Validators.minLength(10)]),
       price: new FormControl(0, [Validators.required, Validators.min(0)]),
-      imageUrl: new FormControl('', Validators.required),
+      image: new FormControl('', Validators.required),
       quantity: new FormControl(0, [Validators.required, Validators.min(0)]),
       available: new FormControl(false, Validators.required),
-      storeName: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      store: new FormControl('', [Validators.required, Validators.minLength(5)]),
 
     });
   }
 
   onSubmit(): void {
     const product: Product = {
-      id: this.uid,
+      uid: this.uid,
       name: (this.productForm.get('name')?.value).trim(),
       description: (this.productForm.get('description')?.value).trim(),
       price: this.productForm.get('price')?.value,
       quantity: this.productForm.get('quantity')?.value,
       available: this.productForm.get('available')?.value,
-      storeName: this.productForm.get('storeName')?.value,
-      imageUrl: this.productForm.get('imageUrl')?.value,
-      createdAt: new Date(),
-      updatedAt: null
+      store: this.productForm.get('store')?.value,
+      image: this.productForm.get('image')?.value,
+      created_at: new Date(),
+      updated_at: null
     };
     console.log(product);
+
+    this.productsService.addProduct(product).subscribe((res) => {
+      console.log('#After ADD=> ', res);
+      this.productForm.reset();
+    });
   }
 
 }
